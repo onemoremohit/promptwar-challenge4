69fb1db8d7d10415cb93e79c10278e23c57b942f
onemoremohit <veerprajapati115@gmail.com>
2026-07-19 21:48:38 +0530

    chore: Switch to GitHub pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/promptwar-challenge4/',
   plugins: [react()],
   test: {
     globals: true,
